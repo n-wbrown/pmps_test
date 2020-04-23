ac24a74f873e2596abbf155c1a443c08ea71fd0b
n-wbrown <n-wbrown@users.noreply.github.com>
2020-04-23 10:15:22 -0700

    DEV/TST: add plugin to standard system tests

--- pmps_test/tests/test_commandline.py
+++ pmps_test/tests/test_commandline.py
@@ -3,5 +3,3 @@ from pmps_test.bin.system_tests import main as system_tests_main
 
 def test_system_tests_main():
     k = system_tests_main(["pmps_test/pmps", "--capture=sys", "-q"])
-    # print(k)
-    assert False
--- pmps_test/utils/pytest_wrapper.py
+++ pmps_test/utils/pytest_wrapper.py
@@ -2,11 +2,19 @@ import pytest
 import typing
 
 
-def targeted_pytest(target_directory: str = None,
+def targeted_pytest(target_directory: str,
                     pytest_args: typing.List = None,
                     pytest_plugins: typing.List = None) -> None:
 
+    args = [f"{target_directory}"]
+    if pytest_args is not None:
+        args.extend(pytest_args)
+
+    plugins = ["pmps_test"]
+    if pytest_plugins is not None:
+        plugins.extend(pytest_plugins)
+
     pytest.main(
-        args=[f"{target_directory}"]+pytest_args,
-        # plugins=pytest_plugins,
+        args=args,
+        plugins=plugins,
     )
