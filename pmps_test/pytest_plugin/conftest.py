10e95bf961f97b3e587307697616d9c23fb743fe
n-wbrown <n-wbrown@users.noreply.github.com>
2020-04-23 17:18:30 -0700

    DEV: Add paramatarized fixtures

--- pmps_test/pytest_plugin/conftest.py
+++ pmps_test/pytest_plugin/conftest.py
@@ -1,8 +1,31 @@
 import pytest
 
+AMS_NET_ID_OPTION = "--ams_net_id"
+
+def pytest_addoption(parser):
+    """
+    This is a built-in pytest hook.
+
+    This function allows os to pass options directly to pytest when run.
+    """
+    parser.addoption(
+        AMS_NET_ID_OPTION, action="store", default=None,
+        help="AMS NET ID adress list passed via command"
+        "line. Supersedes the .ini file"
+    )
+    parser.addini(
+        AMS_NET_ID_OPTION.strip("-"), default=None,
+        help="AMS NET ID address list passed via the .ini file."
+    )
+
+
 @pytest.fixture
-def sample_fixture():
+def ams_net_id(request):
     """
-    Confirm availability of plugin fixtures.
+    Set AMS_NET_ID of target PLC
     """
-    return "This is sample_fixture's string"
+    if request.config.getini(AMS_NET_ID_OPTION) is not None:
+        return request.config.getini(AMS_NET_ID_OPTION)
+    else:
+        return request.config.getoption(AMS_NET_ID_OPTION)
+     
